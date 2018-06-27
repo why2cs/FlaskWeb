343ddaee633e95fe57a5445d46220a3002e1cf5f
why2cs <why2cs@outlook.com>
2018-06-27 12:05:26 +0800

    create respository

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route("/user/<name>")
+def get_user(name):
+    return render_template("user.html", name=name)
+
+
+if __name__ == '__main__':
+    app.run()

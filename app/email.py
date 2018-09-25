4e72e3ca5b77d719b005d637bd4794d9d9a67681
why2cs <why2cs@outlook.com>
2018-09-26 00:29:13 +0800

    chapter 7.3

---
+++ app/email.py
@@ -0,0 +1,22 @@
+from app import app, mail
+
+from flask_mail import Mail, Message
+from threading import Thread
+from flask import render_template
+
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config.FLASKY_MAIL_SUBJECT_PREFIX + subject,
+                  sender=app.config.FLASKY_MAIL_SENDER, recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+
+    thr = Thread(target=send_async_mail, args=[app, msg])
+    thr.start()
+
+    return thr
+
+
+def send_async_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
